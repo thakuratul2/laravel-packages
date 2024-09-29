14306e92fbde8da38e45c9993f42e4d4187d7d94
thakuratul2 <pratapsinghatul111@gmail.com>
2024-09-29 14:55:28 +0530

    Added: Provider Mentioned

---
+++ packages/brsoftsolatul/TimeAttendance/src/Providers/TimeAttendanceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace BRsoftsolatul\TimeAttendance\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class TimeAttendanceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
